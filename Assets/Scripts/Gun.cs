8731b0d1927ad23ceb57be531557f6db9f2956ec
the-cyber-admin <38099115+the-cyber-admin@users.noreply.github.com>
2018-08-08 15:46:15 +0300

    Revert "Fail"
    This reverts commit 0a524c19377f128177b005b0cba2f2473d506a98.

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -13,13 +13,9 @@ public class Gun : MonoBehaviour
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
-<<<<<<< HEAD
 	public float reloadRate = 0.5f;
 
 	float _nextTimeShot;
-=======
-	
->>>>>>> parent of 7ead4d7... Firerate
 =======
 	
 >>>>>>> parent of 7ead4d7... Firerate
@@ -78,7 +74,6 @@ public class Gun : MonoBehaviour
 <<<<<<< HEAD
 		isCharging = false;
 <<<<<<< HEAD
-<<<<<<< HEAD
 		_nextTimeShot = Time.time + reloadRate;
 =======
 		var pro = Instantiate(projectile, shootingPoint.position, Quaternion.identity, shootingPoint);
@@ -97,8 +92,6 @@ public class Gun : MonoBehaviour
 		var pro = Instantiate(projectile, shootingPoint.position, Quaternion.identity, shootingPoint);
 =======
 >>>>>>> parent of 7ead4d7... Firerate
-=======
->>>>>>> parent of 7ead4d7... Firerate
 		pro.GetComponent<ShotController>()
 			.Shoot((shootingPoint.position - center.position).normalized * power);
 	}
@@ -107,7 +100,6 @@ public class Gun : MonoBehaviour
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
-<<<<<<< HEAD
 >>>>>>> parent of dc23c87... Add Jump Limter
 =======
 	
@@ -119,15 +111,10 @@ public class Gun : MonoBehaviour
 =======
 >>>>>>> parent of dc23c87... Add Jump Limter
 =======
-=======
->>>>>>> parent of 7ead4d7... Firerate
 	void Shoot()
 	{
 		
 	}
 	
-<<<<<<< HEAD
->>>>>>> parent of 7ead4d7... Firerate
-=======
 >>>>>>> parent of 7ead4d7... Firerate
 }
