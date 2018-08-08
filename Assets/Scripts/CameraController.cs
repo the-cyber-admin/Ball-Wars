e2fe5774f173c1dc0fab97e9bc8e597fbba979a0
the-cyber-admin <38099115+the-cyber-admin@users.noreply.github.com>
2018-08-08 11:14:18 +0300

    Simple Controllers Added
    Camera Controller
    Gun Rotator
    Death Bound
    --With Objects in The Scene

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class CameraController : MonoBehaviour
+{
+
+	public List<Transform> targets;
+	public Vector3 offset;
+	public float minZoom = 40f;
+	public float maxZoom = 10f;
+	public float smoothTime = 0.5f;
+	public float zoomLimter = 50f;
+
+	private float realSmoothTime;
+	private Camera cam;
+	private Vector3 velocity;
+
+	void Awake()
+	{
+		cam = GetComponent<Camera>();
+	}
+	
+	void LateUpdate ()
+	{
+		if(targets.Count == 0)
+			return;
+		Move();
+		Zoom();
+	}
+
+	void Zoom()
+	{
+		float newZoom = Mathf.Lerp(maxZoom, minZoom,
+			GetLongestDistance() / zoomLimter);
+		cam.fieldOfView = Mathf.Lerp(cam.fieldOfView, newZoom, Time.deltaTime);
+
+	}
+
+	float GetLongestDistance()
+	{
+		var bounds = new Bounds();
+
+		foreach (var target in targets)
+		{
+			if(target != null)
+			bounds.Encapsulate(target.position);
+		}
+
+		return bounds.size.x;
+	}
+	
+	void Move()
+	{
+		Vector3 center = GetCenterPoint();
+		Vector3 newPosition = center + offset;
+		transform.position = Vector3.SmoothDamp(transform.position, newPosition, ref velocity, smoothTime);
+	}
+
+	Vector3 GetCenterPoint()
+	{
+		if (targets.Count == 0)
+		{
+			return targets[0].position;
+		}
+		
+		var bounds = new Bounds();
+		
+		foreach (var t in targets)
+		{
+			if(t != null)
+			bounds.Encapsulate(t.position);
+		}
+
+		return bounds.center;
+	}
+}
---
+++ Assets/Scripts/GunRotator.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class GunRotator : MonoBehaviour
+{
+
+	public Transform center;
+	public int invert = 1;
+	
+	Camera cam;
+	void Awake()
+	{
+		cam = Camera.main;
+	}
+	
+	void Update () 
+	{
+		//Get the Screen positions of the object
+		Vector2 positionOnScreen = cam.WorldToViewportPoint (center.position);
+         
+		//Get the Screen position of the mouse
+		Vector2 mouseOnScreen = cam.ScreenToViewportPoint(Input.mousePosition);
+         
+		//Get the angle between the points
+		float angle = AngleBetweenTwoPoints(positionOnScreen, mouseOnScreen);
+ 
+		//Assign the new Rotation
+		center.rotation =  Quaternion.Euler (new Vector3(0f,0f,angle));
+	}
+ 
+	float AngleBetweenTwoPoints(Vector3 a, Vector3 b)
+	{
+		a *= invert;
+		b *= invert;
+		
+		return Mathf.Atan2(a.y - b.y, a.x - b.x) * Mathf.Rad2Deg;
+	}
+}
