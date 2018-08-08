d6a668818ffc6cc5dfc8cdb9a2d840d1391fad3e
the-cyber-admin <38099115+the-cyber-admin@users.noreply.github.com>
2018-08-08 12:54:37 +0300

    Add damage control
    Player hits an opponent increase the opponent damaged health and become
    more vurnearable

---
+++ Assets/Scripts/Damageable.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Damageable : MonoBehaviour
+{
+
+	public float damageMultiply = 10f;
+	float _damage = 1f;
+	
+	Rigidbody rb;
+	void Awake()
+	{
+		rb = GetComponent<Rigidbody>();
+	}
+
+
+	public void AddDamage(Vector3 dir, float damage)
+	{
+		_damage += damage;
+		transform.localScale = new Vector3(1f,1f,1f) * _damage;
+		rb.AddForce(_damage * damageMultiply * dir);
+	}
+}
--- Assets/Scripts/ShotController.cs
+++ Assets/Scripts/ShotController.cs
@@ -3,22 +3,36 @@
 public class ShotController : MonoBehaviour
 {
     public float lifetime = 3f;
-
+    public float damage = 0.1f;
+    
     Rigidbody rb;
-
     void Awake()
     {
         rb = GetComponent<Rigidbody>();
     }
-    
-    void Start()
-    {
-        transform.SetParent(null);
-    }
 
     public void Shoot(Vector3 power)
     {
+        transform.SetParent(null);
         Destroy(gameObject , lifetime);
         rb.AddForce(power);
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        var hit = other.gameObject.GetComponent<Damageable>();
+        if (hit != null)
+        {
+            hit.AddDamage(
+                (other.transform.position - transform.position).normalized ,
+                damage);
+        }
+        
+        Explode();
+    }
+
+    void Explode()
+    {
+        Destroy(gameObject);
     }
 }
